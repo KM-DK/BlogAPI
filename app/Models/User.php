9f0b91997131dbc7d0a464337d265b0ea7d076e0
unknown <krzysztofmichalski987@gmail.com>
2021-08-03 09:08:51 +0200

    add more user fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,37 +7,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name',
+        'surname',
+        'account_name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $attributes = [
+        'is_active' => false,
+    ];
+
+    public function role() {
+        return $this->hasOne(DictionaryTerm::class);
+    }
 }
